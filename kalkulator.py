d99b2993078d3da4708c9e2973adef7ff14591cb
objectGrzegorz <pocztamilanowski@gmail.com>
2019-10-30 12:03:49 +0100

    Dodatnie prostego kalkulatora

---
+++ kalkulator.py
@@ -0,0 +1,12 @@
+def dodaj(a,b):
+    return a+b
+def odejmij(a,b):
+    return a-b
+def pomnoz(a,b):
+    return a*b
+def podziel(a,b):
+    if (b==0):
+        print("B musi być różne od zera!")
+        return 0
+    return a/b
+
