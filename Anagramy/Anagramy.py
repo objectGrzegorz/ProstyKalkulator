5d288776cafc4a70dda6d6db529726e1794b9cd0
objectGrzegorz <pocztamilanowski@gmail.com>
2019-11-22 22:25:51 +0100

    poprawka

--- Anagramy/Anagramy.py
+++ Anagramy/Anagramy.py
@@ -1,15 +1,19 @@
 def slownik():
     with open("words.txt", "r") as plik:
-        dict = {}
+        dict2 = {}
         slowo = plik.readline().strip()
         while slowo != "":
             k = tuple(sorted(slowo))
             lista = [slowo]
-            if k not in dict:
-                dict[k] = lista
+            if k not in dict2:
+                dict2[k] = lista
             else:
-                dict[k] += lista
+                dict2[k] += lista
             slowo = plik.readline().strip()
+        dict={}
+        for k,v in dict2.items():
+            if len(v)>1:
+                dict[k]=v
         return dict
 
 def lista_anagramow(dict):
@@ -28,4 +32,15 @@ def od_najdluzszej(dict):
     for k,v in lista:
         print(v)
 
-od_najdluzszej(dict)
+def bingo(dict):
+    slownik2 = {}
+    for k, v in dict.items():
+        slownik2[tuple(v)] = len(v)
+    lista = []
+    for k, v in slownik2.items():
+        lista.append((v, k))
+    lista.sort(reverse=True)
+    for k, v in lista:
+        if len(v) == 8:
+            print(v)
+bingo(dict)
